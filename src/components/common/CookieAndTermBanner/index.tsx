52f8bf08592bdf44644aa8e76a0a1c558002a8fc
ajimeno04 <jimenobeltran11@gmail.com>
2024-11-20 14:00:34 +0100

    deletion: unused imports

--- src/components/common/CookieAndTermBanner/index.tsx
+++ src/components/common/CookieAndTermBanner/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, type ReactElement } from 'react'
 import classnames from 'classnames'
 import type { CheckboxProps } from '@mui/material'
-import { Grid, Button, Checkbox, FormControlLabel, Typography, Paper, SvgIcon, Box } from '@mui/material'
+import { Grid, Button, Checkbox, FormControlLabel, Typography, Paper, SvgIcon, Box, Link } from '@mui/material'
 import WarningIcon from '@/public/images/notifications/warning.svg'
 import { useForm } from 'react-hook-form'
 import { metadata } from '@/markdown/terms/terms.md'
@@ -17,7 +17,6 @@ import { selectCookieBanner, openCookieBanner, closeCookieBanner } from '@/store
 
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
-import ExternalLink from '../ExternalLink'
 
 const COOKIE_AND_TERM_WARNING: Record<CookieAndTermType, string> = {
   [CookieAndTermType.TERMS]: '',
@@ -87,8 +86,13 @@ export const CookieAndTermBanner = ({
           <Grid item xs>
             <Typography variant="body2" mb={2}>
               By browsing this page, you accept our{' '}
-              <ExternalLink href={AppRoutes.terms}>Terms & Conditions</ExternalLink> and the use of necessary cookies.
-              <ExternalLink href={AppRoutes.cookie}>Cookie policy</ExternalLink>
+              <Link href={AppRoutes.terms}>
+                <u>Terms & Conditions</u>
+              </Link>{' '}
+              and the use of necessary cookies.
+              <Link href={AppRoutes.cookie}>
+                <u>Cookie policy</u>
+              </Link>
             </Typography>
 
             <Grid container alignItems="center" gap={4}>
