e7f92573dbcf452122df51a6e63ec96983ec5688
Den Smalonski <dzianis.smalonski@protofire.io>
2025-02-07 13:19:39 +0530

    fix: merge conflicts

--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,7 +1,5 @@
 import { TERMS_LINK } from '@/config/constants'
 import { Typography } from '@mui/material'
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
@@ -25,4 +23,4 @@ const SafeTerms = () => {
   return <main>{content ? <ReactMarkdown>{content}</ReactMarkdown> : <Typography>Loading terms...</Typography>}</main>
 }
 
-export default Terms
+export default SafeTerms
