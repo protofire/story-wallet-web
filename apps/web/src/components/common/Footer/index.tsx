d730978393215d03300158acb048298690225664
ajimeno04 <jimenobeltran11@gmail.com>
2025-04-01 11:33:29 +0200

    chore: enhance footer by adding Cookie policy and Terms links, and clean up commented code

--- apps/web/src/components/common/Footer/index.tsx
+++ apps/web/src/components/common/Footer/index.tsx
@@ -1,6 +1,8 @@
 import type { ReactElement, ReactNode } from 'react'
 import { SvgIcon, Typography } from '@mui/material'
 import { useRouter } from 'next/router'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import packageJson from '../../../../package.json'
 import css from './styles.module.css'
 import { AppRoutes } from '@/config/routes'
 import ExternalLink from '../ExternalLink'
@@ -66,21 +68,20 @@ const Footer = (): ReactElement | null => {
               <FooterLink href={getHref(AppRoutes.settings.index)}>Preferences</FooterLink>
             </li> */}
         <li>
-          <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
-            Help
-          </ExternalLink>
-        </li>
-
-        {/* <li>
-          <ExternalLink href={`${packageJson.homepage}/releases/tag/v${packageJson.version}`} noIcon>
-            <SvgIcon component={GitHubIcon} inheritViewBox fontSize="inherit" sx={{ mr: 0.5 }} /> v{packageJson.version}
-          </ExternalLink>
-        </li> */}
-        <li>
           <FooterLink href={getHref(AppRoutes.cookie)}>Cookie policy</FooterLink>
         </li>
         <li>
           <FooterLink href={getHref(AppRoutes.terms)}>Terms</FooterLink>
+        </li>
+        <li>
+          <ExternalLink href={HELP_CENTER_URL} noIcon sx={{ span: { textDecoration: 'underline' } }}>
+            Help
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink href={`${packageJson.homepage}/releases/tag/v${packageJson.version}`} noIcon>
+            <SvgIcon component={GitHubIcon} inheritViewBox fontSize="inherit" sx={{ mr: 0.5 }} /> v{packageJson.version}
+          </ExternalLink>
         </li>
         <li>
           <Typography variant="caption">
